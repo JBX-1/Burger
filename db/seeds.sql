b8dbeb83b38a7161663bf3ab212836c4fa9fd3b4
Messiah <Taylor.Jonathan@aol.com>
2018-10-15 21:13:43 -0400

    12

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Veggie Burger');
+INSERT INTO burgers (burger_name) VALUES ('Angus Beef');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Soy Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Turkey Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Black Bean Burger', true);
+INSERT INTO burgers (burger_name) VALUES ('Tofu Burger');
